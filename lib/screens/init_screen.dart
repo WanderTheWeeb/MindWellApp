82983e5aa18c3949819f2084da30037928cb3b8a
WandeTheWeeb <josefranciscosantesprimo@gmail.com>
2024-02-15 19:51:54 -0600

    add buttonImage Widget v1

--- lib/screens/init_screen.dart
+++ lib/screens/init_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mindwell/routes/routes.dart';
+import 'package:mindwell/widgets/button_banner.dart';
 
 class InitScreen extends StatelessWidget {
   const InitScreen({super.key});
@@ -7,20 +8,47 @@ class InitScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MindWell'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {
-              Navigator.of(context).pushNamed(Routes.settings);
-            },
+        appBar: AppBar(
+          title: const Text('MindWell'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                Navigator.of(context).pushNamed(Routes.settings);
+              },
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ImageButton(
+                margin: const EdgeInsets.all(20),
+                borderRadius: BorderRadius.circular(10),
+                imagePath:
+                    'https://imgs.search.brave.com/vMWJriFuJ7muG5_KaGHFhYCniZq_Om3TrNYzFKHlk3k/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTMw/NTE5NDg1MC9waG90/by93b21hbi1wcmFj/dGljaW5nLXlvZ2Et/YXQtaG9tZS5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9anFK/Wmx6NXlnY1o2LS1s/cVNZdVBvMk1XSWxS/cmtRa3pINmtzUjJi/WnFTcz0',
+                onPressed: () {
+                  //Navigator.of(context).pushNamed(Routes.meditation);
+                },
+                buttonText: 'Hora de yoga',
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                '¡Bienvenido a MindWell!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                '¡Comienza a meditar ahora!',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Hello, World!'),
-      ),
-    );
+        ));
   }
 }
