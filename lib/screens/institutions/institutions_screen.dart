b4f2534a99616deb79180adde3f9c23394815fc2
WanderTheWeeb <josefranciscosantesprimo@gmail.com>
2024-02-21 10:49:30 -0600

    app bar

--- lib/screens/institutions/institutions_screen.dart
+++ lib/screens/institutions/institutions_screen.dart
@@ -9,28 +9,31 @@ class InstitutionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('Instituciones'),
+        ),
         body: SafeArea(
-      child: GridView.count(
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        crossAxisCount: 2,
-        padding: const EdgeInsets.all(15),
-        children: [
-          InstitutionCard(
-              onPressed: () {
-                Navigator.pushNamed(context, Routes.institutionProfile);
-              },
-              name: 'Cendiuh',
-              pathImage:
-                  'https://www.uv.mx/coatza/usbi/files/2020/11/cabeza-olmeca-usbi.jpg'),
-          InstitutionCard(
-            onPressed: () {},
-            name: 'Psicólogo-Psicoterapeuta Marío Alberto Gómez Cuervo',
-            pathImage:
-                'https://lh5.googleusercontent.com/p/AF1QipP0sFs5KmGo8ffdD050_mVf7lbULwBYbYngOCfp=w426-h240-k-no',
-          )
-        ],
-      ),
-    ));
+          child: GridView.count(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            crossAxisCount: 2,
+            padding: const EdgeInsets.all(15),
+            children: [
+              InstitutionCard(
+                  onPressed: () {
+                    Navigator.pushNamed(context, Routes.institutionProfile);
+                  },
+                  name: 'Cendiuh',
+                  pathImage:
+                      'https://www.uv.mx/coatza/usbi/files/2020/11/cabeza-olmeca-usbi.jpg'),
+              InstitutionCard(
+                onPressed: () {},
+                name: 'Psicólogo-Psicoterapeuta Marío Alberto Gómez Cuervo',
+                pathImage:
+                    'https://lh5.googleusercontent.com/p/AF1QipP0sFs5KmGo8ffdD050_mVf7lbULwBYbYngOCfp=w426-h240-k-no',
+              )
+            ],
+          ),
+        ));
   }
 }
